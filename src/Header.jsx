be49f02b50620bb8bbad6073e4f961303b3dbe7a
datkhac009 <datkhac009@gmail.com>
2025-11-03 09:56:49 +0700

    Done

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@ export function Header() {
   return (
     <>
       <header className="app-header">
-        <img src="./src/assets/quiz-avartar.png" alt="React" />
+        <img src="public/quiz-avartar.png" alt="React" />
         <h1>Welcome to my Quiz</h1>
       </header>
     </>
