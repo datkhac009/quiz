f02de3c7103436dae4a0c1a50824e40fa0af4cf9
datkhac009 <datkhac009@gmail.com>
2025-11-03 10:45:27 +0700

    Done

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@ export function Header() {
   return (
     <>
       <header className="app-header">
-        <img src="public/quiz-avartar.png" alt="React" />
+        <img src="./src/assets/quiz-avartar.png" alt="React" />
         <h1>🎮 Welcome to the Ultimate Game Quiz!</h1>
       </header>
     </>
